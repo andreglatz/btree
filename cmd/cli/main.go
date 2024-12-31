a069234be4ea39f59b1c6213dbcd8baa7bb25a1e
André Glatz <andreglatz@outlook.com>
2024-12-31 14:53:11 -0300

    optimize memory usage

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -9,22 +9,22 @@ import (
 
 func main() {
 	tree := graph.NewNode(1, graph.NewNode(2, graph.NewNode(4), graph.NewNode(5)), graph.NewNode(3, graph.NewNode(6)))
-	q := queue.NewQueue[[]*graph.Node[int]]()
+	q := queue.NewQueue[*graph.Node[int]]()
 
-	q.Enqueue([]*graph.Node[int]{tree})
+	q.Enqueue(tree)
 
 	for q.Size() > 0 {
-		nodes := q.Dequeue()
-		var level []*graph.Node[int]
+		levelSize := q.Size()
 
-		for _, v := range nodes {
-			fmt.Print(v.Value, " ")
-			level = append(level, v.Children...)
+		for i := 0; i < int(levelSize); i++ {
+			node := q.Dequeue()
+			fmt.Print(node.Value, " ")
+
+			for _, child := range node.Children {
+				q.Enqueue(child)
+			}
 		}
+
 		fmt.Println()
-
-		if len(level) > 0 {
-			q.Enqueue(level)
-		}
 	}
 }
